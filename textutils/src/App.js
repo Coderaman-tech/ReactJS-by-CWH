2899ed2366e5b912ba24e53632d18210512716ce
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2023-02-26 23:13:03 +0530

    add TextForm

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils" aboutText="About"/>
+    <div className='container my-3'>
+      <TextForm heading="Enter the text to analyze below"/>
+      <button className='btn btn-primary'>Convert to Uppercase</button>
 
-    {/* check for default props */}
-
-    {/* <Navbar/> */}
-
+    </div>
     </>
   );
 }
